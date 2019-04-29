9702797f5551dbeb7fae0ffd031c9ca440647a15
Carl Zhang <356889@gmail.com>
2019-04-29 10:43:05 +0800

    Release 0.1.3

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
-(defproject lein-rpmbuild "0.1.3-SNAPSHOT"
+(defproject lein-rpmbuild "0.1.3"
   :description "Leiningen plugin for building an RPM package"
   :url "http://github.com/carlzhc/lein-rpmbuild"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :eval-in-leiningen true)
-
+  :eval-in-leiningen true
+  :dependencies [[carlzhc/lein-tar "3.3.0.1"]])
